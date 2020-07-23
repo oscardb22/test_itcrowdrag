2907720d4492fadc640b9fb5389e51a54a02ef71
oscardb22 <oscardb22@gmail.com>
2020-07-23 15:04:57 -0500

    heroku requirements

--- test_itcrowdrag/settings.py
+++ test_itcrowdrag/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from pathlib import Path
+from dotenv import load_dotenv
+
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
