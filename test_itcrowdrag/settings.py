387f9f20dbacdd3dd886379b79deea77b9095a3c
oscardb22 <oscardb22@gmail.com>
2020-07-23 16:53:30 -0500

    heroku requirements

--- test_itcrowdrag/settings.py
+++ test_itcrowdrag/settings.py
@@ -54,12 +54,14 @@ INSTALLED_APPS = [
     # LIBRARIES
     'rest_framework',
     'rest_framework.authtoken',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     # 'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
