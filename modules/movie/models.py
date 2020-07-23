ecf7e984e4b6e533a596fdd43de0f0e1df4e19f4
oscardb22 <oscardb22@gmail.com>
2020-07-22 21:00:01 -0500

    - create a requirements.txt file it's content are all the libraries that i use in this moment. - i made and setup models that i consider necessary. - created some environment variable for the settings.py security.

--- modules/movie/models.py
+++ modules/movie/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.conf import settings
+from modules.person.models import Persons
 
-# Create your models here.
+
+class Movies(models.Model):
+    tittle = models.CharField(max_length=50)
+    release_year = models.CharField(max_length=5)
+    casting = models.ManyToManyField(Persons, null=True)
+    directors = models.ManyToManyField(Persons, null=True)
+    producers = models.ManyToManyField(Persons, null=True)
+    pro = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return f"{self.tittle} {self.release_year}"
--- modules/person/models.py
+++ modules/person/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Persons(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    aliases = models.CharField(max_length=50)
+    pro = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
