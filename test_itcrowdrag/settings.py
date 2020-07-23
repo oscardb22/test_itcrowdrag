20e9c9d949683046b76c8d37490fbc94e4646d23
oscardb22 <oscardb22@gmail.com>
2020-07-23 17:00:39 -0500

    heroku requirements

--- test_itcrowdrag/settings.py
+++ test_itcrowdrag/settings.py
@@ -167,3 +167,9 @@ REST_FRAMEWORK = {
 
 # WHITENOISE
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# CORSHEADERS
+CORS_ORIGIN_ALLOW_ALL = True
+"""
+CORS_ORIGIN_WHITELIST = ["http://localhost:3000", http://localhost:8000]
+"""
